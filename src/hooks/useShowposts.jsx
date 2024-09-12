cf5418a7d15109f3100d366158e3078b50e43dca
Ravikant <ravidreamer7@gmail.com>
2024-09-13 00:46:29 +0530

    "console"

--- src/hooks/useShowposts.jsx
+++ src/hooks/useShowposts.jsx
@@ -8,15 +8,15 @@ const useShowposts = () => {
     setLoading(true);
     try {
       const token = localStorage.getItem("jwt");
-      
+
       const response = await axios.get(
         `${import.meta.env.VITE_APP_URL}/api/post/allposts`,
         { withCredentials: true }
-        
       );
+      console.log("response: ", response);
 
       const data = response.data; // Axios parses the response automatically
-      
+
       return data?.allposts; // Return the relevant part of the data
     } catch (error) {
       toast.error(error.message); // Display error message
